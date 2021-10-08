b0c0aa8ddc5addac13efce0523b9ae261e6ea980
Sasha <q.dev.null@gmail.com>
2021-10-08 14:14:14 +0200

    Rename the modules

--- setup.py
+++ setup.py
@@ -1,12 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="detectors-cli",
+    name="coin-detectors",
     version="0.0.1",
     packages=find_packages(),
-    entry_points={
-        'console_scripts': [
-            'solution=model.main:main',
-        ],
-    },
 )
