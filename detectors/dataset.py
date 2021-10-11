46d02fbd586678d3815ab09348379dfdd705bc39
Sasha <q.dev.null@gmail.com>
2021-10-11 13:46:40 +0200

    Fix a linting error

--- detectors/dataset.py
+++ detectors/dataset.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import torch
 
+
 def read_image(file):
     bitmap = cv2.imread(file, cv2.IMREAD_GRAYSCALE)
     image = np.stack([bitmap, bitmap, bitmap]).astype('float32')
