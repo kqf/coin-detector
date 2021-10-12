940290ccfe8838919bd3fcec39831c9041f0dee3
Sasha <q.dev.null@gmail.com>
2021-10-12 09:13:33 +0200

    Add meaningful tests

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -6,5 +6,7 @@ def test_dataset(fake_dataset):
     df = pd.read_csv(fake_dataset / "train.csv")
     data = DetectionDataset(df)
 
-    for batch in data:
-        print(batch)
+    for image, boxes, labels in data:
+        assert len(image.shape) == 3
+        assert len(boxes.shape) == 2
+        assert len(labels.shape) == 1
