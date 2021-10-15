559b3a22c4094423bc508d62d9e6e881eaffc013
Sasha <q.dev.null@gmail.com>
2021-10-15 17:05:41 +0200

    Fix the conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -73,7 +73,7 @@ def annotations(fixed_seed, tmp_path, width=2000, num_classes=3, n_samples=8):
 
 
 @pytest.fixture
-def fake_dataset(tmp_path, annotations, size=256):
+def fake_dataset(tmp_path, annotations, size=256, image_col="image_id"):
     for image_id, blobs in annotations.groupby(image_col):
 
         blobs = []
