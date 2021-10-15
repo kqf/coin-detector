1852f34799980272f3b9cef2c85b98f07ec5b3df
kqf <q.dev.null@gmail.com>
2021-10-15 17:11:45 +0200

    Turn on all augmentations (#15)
    * Fix the conftest
    
    * Remove the unused imports
    
    * Add the flips

--- detectors/augmentations.py
+++ detectors/augmentations.py
@@ -32,11 +32,11 @@ def transform(train=True, mean=None, std=None, size=32 * 13):
     std = std or _std
 
     transforms = [
-        # alb.PadIfNeeded(
-        #     min_height=int(size),
-        #     min_width=int(size),
-        #     border_mode=cv2.BORDER_CONSTANT,
-        # ),
+        alb.PadIfNeeded(
+            min_height=int(size),
+            min_width=int(size),
+            # border_mode=cv2.BORDER_CONSTANT,
+        ),
         # DebugAugmentations(),
         alb.Resize(size, size),
         alb.Normalize(mean=mean, std=std, max_pixel_value=255.0, p=1.0),
@@ -46,7 +46,7 @@ def transform(train=True, mean=None, std=None, size=32 * 13):
     train_transforms = []
     if train:
         train_transforms = [
-            # alb.Flip(0.5)
+            alb.Flip(0.5)
         ]
 
     return alb.Compose(train_transforms + transforms)
