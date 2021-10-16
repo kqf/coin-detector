bb343dd82698a65d59c7c65e5e3a9b5478aad433
Sasha <q.dev.null@gmail.com>
2021-10-16 09:44:04 +0200

    Fix the broken kernel sizes

--- detectors/model.py
+++ detectors/model.py
@@ -1,18 +1,18 @@
 import torch
 
 
-def default_heads(n_classes):
+def default_heads(n_classes, kernel_size=6):
     return torch.nn.ModuleDict({
-        "boxes": torch.nn.Conv2d(100, 4),
-        "classes": torch.nn.Conv2d(100, n_classes)
+        "boxes": torch.nn.Conv2d(100, 4, kernel_size),
+        "classes": torch.nn.Conv2d(100, n_classes, kernel_size)
     })
 
 
 class DummyDetector(torch.nn.Module):
-    def __init__(self, heads=None, n_classes=2):
+    def __init__(self, heads=None, n_classes=2, kernel_size=6):
         super().__init__()
-        self.backbone = torch.nn.AdaptiveAvgPool2d(6)
-        self.heads = heads or default_heads(n_classes)
+        self.backbone = torch.nn.AdaptiveAvgPool2d(kernel_size)
+        self.heads = heads or default_heads(n_classes, kernel_size)
 
     def forward(self, x):
         latent = self.backbone(x)
