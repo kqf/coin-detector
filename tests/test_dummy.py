1efc5727437a69a3953437d1742f2fc0549db4a4
Sasha <q.dev.null@gmail.com>
2021-10-16 09:32:22 +0200

    Check if the dummy model works

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -1,4 +1,13 @@
+import pytest
+import torch
+from detectors.model import DummyDetector
 
 
-def test_dummy(fake_dataset):
-    pass
+@pytest.fixture
+def batch(batch_size=16):
+    return torch.zeros(batch_size, 3, 28, 28)
+
+
+def test_dummy(batch):
+    model = DummyDetector()
+    model(batch)
