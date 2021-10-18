d94badc1b5af6461f1060141dc002be977f5c2b5
Sasha <q.dev.null@gmail.com>
2021-10-18 15:40:23 +0200

    Add the tests for the model

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,5 +1,12 @@
+import pandas as pd
 from detectors.model import build_model
+from detectors.dataset import DetectionDataset
 
 
+@pytest.mark.xfail
 def test_model(fake_dataset):
-    print(fake_dataset)
+    df = pd.read_csv(fake_dataset / "train.csv")
+    train = DetectionDataset(df)
+
+    model = build_model()
+    model.fit(train)
