675a84fd78b019ea80cd7c6fdec6cb81cf425027
Sasha <q.dev.null@gmail.com>
2021-10-19 08:54:41 +0200

    Add the losses

--- detectors/loss.py
+++ detectors/loss.py
@@ -1,10 +1,22 @@
 import torch
 
 
+def default_losses():
+    losses = {
+        "boxes": torch.nn.MSELoss,
+        "classes": torch.nn.CrossEntropyLoss,
+    }
+    return losses
+
+
 class DetectionLoss(torch.nn.Module):
     def __init__(self, sublosses=None):
         super().__init__()
-        self.sublosses = sublosses
+        self.sublosses = sublosses or default_losses()
 
     def forward(self, y_pred, y):
-        return 0.1
+        losses = []
+        for name, subloss in self.sublosses.items():
+            import ipdb; ipdb.set_trace(); import IPython; IPython.embed()  # noqa
+            losses.append(subloss(y_pred[name], y[name]))
+        return torch.cat(losses).sum()
