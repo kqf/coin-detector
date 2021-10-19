5c718f737520423c8c3d47853a781e776bd790a0
Sasha <q.dev.null@gmail.com>
2021-10-19 09:39:19 +0200

    Clean the model tests

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,10 +1,8 @@
-import pytest
 import pandas as pd
 from detectors.model import build_model
 from detectors.dataset import DetectionDataset
 
 
-# @pytest.mark.xfail
 def test_model(fake_dataset):
     df = pd.read_csv(fake_dataset / "train.csv")
     train = DetectionDataset(df)
