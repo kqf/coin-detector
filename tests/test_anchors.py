edcbb9a58260c1b0af5fc1c30cefa8ee7a310c2a
Sasha <q.dev.null@gmail.com>
2021-10-20 11:22:06 +0200

    Fix the anchor definitions

--- tests/test_anchors.py
+++ tests/test_anchors.py
@@ -1,6 +1,6 @@
 import pytest
 import torch
-from detectors.anchors import AnchorBoxes
+from detectors.anchors import DEFAULT_ANCHORS, AnchorBoxes
 
 
 @pytest.fixture
@@ -9,11 +9,17 @@ def image_shape():
 
 
 @pytest.fixture
-def features(image_shape, batch_size=16, channels=8):
+def features(image_shape, batch_size=16, channels=3):
     layer_h, layer_w = image_shape[0] // 2, image_shape[1] // 2
     return torch.zeros((batch_size, channels, layer_h, layer_w))
 
 
 def test_anchors(image_shape, features):
     boxlayer = AnchorBoxes()
-    boxlayer(image_shape, [features])
+    boxes = boxlayer(image_shape, [features])
+    batch_size, n_anchors, n_coords = boxes.shape
+    assert n_coords == 6
+
+    exp_batch_size, _, features_h, features_w = features.shape
+    assert n_anchors == len(DEFAULT_ANCHORS[0]) * features_w * features_h
+    assert batch_size == exp_batch_size
