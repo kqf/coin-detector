fe764bb2ac98615a616f14d4569f9038818332a7
Sasha <q.dev.null@gmail.com>
2021-10-20 09:18:11 +0200

    Add anchors definition

---
+++ detectors/anchors.py
@@ -0,0 +1,78 @@
+import torch
+from functools import reduce
+from operator import mul
+
+DEFAULT_ANCHORS = [
+    [
+        (10, 10),
+        (5, 10),
+        (10, 5),
+    ]
+]
+
+
+class AnchorBoxes(torch.nn.Module):
+    def __init__(self, anchors=None):
+        super().__init__()
+        self.anchors = anchors or DEFAULT_ANCHORS
+
+    def forward(self, image_shape, layers):
+        out_layers = []
+        for idx, layer in enumerate(layers):
+            b, _c, *layer_shape = layer.shape
+            num_anchors = reduce(mul, layer_shape, 1)
+
+            strides = torch.tensor(image_shape) / torch.tensor(layer_shape)
+            offsets = strides / 2
+
+            # create meshgrid
+            cell_y, cell_x = torch.meshgrid(
+                torch.arange(layer_shape[0]),
+                torch.arange(layer_shape[1]),
+            )
+
+            # create heights and widths
+            anchors = []
+            for (anchor_height, anchor_width) in self.anchors[idx]:
+                height = torch.ones(layer_shape) * anchor_height
+                width = torch.ones(layer_shape) * anchor_width
+
+                x_coords = cell_x * strides[1] + offsets[1]
+                y_coords = cell_y * strides[0] + offsets[0]
+
+                on_x = torch.logical_and(
+                    x_coords >= width / 2,
+                    x_coords < image_shape[1] - width / 2,
+                )
+                on_y = torch.logical_and(
+                    y_coords >= height / 2,
+                    y_coords < image_shape[0] - height / 2,
+                )
+
+                # create a lookup for which anchor comes from which layer
+                idx_tensor = torch.ones(layer_shape) * idx
+
+                # outs[layer_h, layer_w, 6]
+                outs = torch.stack(
+                    [
+                        idx_tensor,
+                        torch.logical_and(on_x, on_y),
+                        x_coords,
+                        y_coords,
+                        width,
+                        height,
+                    ],
+                    dim=-1,
+                ).reshape(num_anchors, -1)
+
+                # outs[layer_h, layer_w, 6] -> outs[layer_h * layer_w, 6]
+                anchors.append(outs.reshape(num_anchors, -1))
+            # list of num_layers tensors [n_anchors * layer_h * layer_w, 6]
+            out_layers.append(torch.cat(anchors, dim=0))
+
+        # single_example[n_layers * n_anchors * h * w, 6]
+        single_example = torch.cat(out_layers, dim=0)
+
+        # batch[batch_size, n_layers * n_anchors * h * w, 6]
+        batch = single_example.repeat((b, 1, 1))
+        return batch
