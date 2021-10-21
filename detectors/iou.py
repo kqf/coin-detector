3c3a58dc8074fc3b520e3c5b9ead2eae5c877d16
kqf <q.dev.null@gmail.com>
2021-10-21 15:02:23 +0200

    Add iou (#30)
    * Add the iou implementation
    
    * Add the IoU tests
    
    * Pinpoint the examples
    
    * Check invariant with respect to coordinate shifts
    
    * Suppress the clone warning

---
+++ detectors/iou.py
@@ -0,0 +1,30 @@
+import torch
+
+
+def box2area(bbox, eps=1e-16):
+    # Asumming the bbox format is [x1, y1, x2, y2]
+    width = bbox[..., 2] - bbox[..., 0] + eps
+    hegiht = bbox[..., 3] - bbox[..., 1] + eps
+    return width * hegiht
+
+
+def iou(y_pred, y_true, eps=1e-16):
+    # Don't propagate gradients for this
+    y_pred = y_pred.clone().detach()
+
+    # The bbox areas
+    area_pred = box2area(y_pred)
+    area_true = box2area(y_true)
+
+    # Intersection co-ordinates
+    x1_i = torch.maximum(y_pred[..., 0], y_true[..., 0])
+    y1_i = torch.maximum(y_pred[..., 1], y_true[..., 1])
+    x2_i = torch.minimum(y_pred[..., 2], y_true[..., 2])
+    y2_i = torch.minimum(y_pred[..., 3], y_true[..., 3])
+
+    intersection = torch.relu(x2_i - x1_i) * torch.relu(y2_i - y1_i)
+
+    union = area_pred + area_true - intersection
+    iou_ = intersection / (union + eps)
+
+    return iou_.clamp(0)
