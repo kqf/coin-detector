12ea8d7ba4d0756f6654b6c096474e1608b22fac
Sasha <q.dev.null@gmail.com>
2021-10-21 14:57:37 +0200

    Check invariant with respect to coordinate shifts

--- tests/test_iou.py
+++ tests/test_iou.py
@@ -35,6 +35,11 @@ from detectors.iou import iou
     lambda x: torch.tensor([x]),
     lambda x: torch.tensor([x] * 16),
 ])
-def test_iou(x1, x2, answer, to_batch):
-    b1, b2 = to_batch(x1), to_batch(x2)
+@pytest.mark.parametrize("shift", [0, 1, -1, 2, -2])
+def test_iou(x1, x2, answer, to_batch, shift):
+    b1, b2 = to_batch(x1) + shift, to_batch(x2) + shift
+    # Check if calculates properly
     torch.testing.assert_allclose(iou(b1, b2), to_batch(answer))
+
+    # Check if is symmetric
+    torch.testing.assert_allclose(iou(b2, b1), to_batch(answer))
