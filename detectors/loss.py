167d8d80227332a4b0de953e13ce54bc5d8a0047
Sasha <q.dev.null@gmail.com>
2021-10-23 18:54:51 +0200

    Skip the broken tests

--- detectors/loss.py
+++ detectors/loss.py
@@ -22,7 +22,8 @@ def select(y_pred, y_true, anchor, positives, negatives, use_negatives=False):
     anchor_neg = anchor[torch.where(negatives)]
 
     # Zero is a background
-    y_true_neg = torch.zeros_like(y_pred_neg.sum(-1))
+    import ipdb; ipdb.set_trace(); import IPython; IPython.embed()  # noqa
+    y_true_neg = torch.zeros_like(y_pred_neg.sum(-1, keepdims=True))
 
     y_pred_tot = torch.cat([y_pred_pos, y_pred_neg], dim=0)
     anchor_tot = torch.cat([anchor_pos, anchor_neg], dim=0)
