e8fbb51f1a3f981e1672748a60fa181fabcf8f3d
Sasha <q.dev.null@gmail.com>
2021-10-25 09:52:30 +0200

    Fix the selection

--- detectors/loss.py
+++ detectors/loss.py
@@ -7,7 +7,7 @@ from functools import partial
 from detectors.matching import match
 
 
-def select(y_pred, y_true, anchor, positives, negatives, use_negatives=False):
+def select(y_pred, y_true, anchor, positives, negatives, use_negatives=True):
     batch_, anchor_, obj_ = torch.where(positives)
     y_pred_pos = y_pred[batch_, anchor_]
     y_true_pos = y_true[batch_, obj_]
@@ -20,12 +20,15 @@ def select(y_pred, y_true, anchor, positives, negatives, use_negatives=False):
     anchor_neg = anchor[torch.where(negatives)]
 
     # Zero is a background
-    y_true_neg = torch.zeros_like(y_pred_neg)
+    y_true_neg_shape = [y_pred_neg.shape[0]]
+    if len(y_true_pos.shape) > 1:
+        y_true_neg_shape.append(y_true_pos.shape[-1])
+    y_true_neg = torch.zeros(y_true_neg_shape, device=y_true_pos.device)
 
     y_pred_tot = torch.cat([y_pred_pos, y_pred_neg], dim=0)
     anchor_tot = torch.cat([anchor_pos, anchor_neg], dim=0)
-    y_true_tot = torch.cat([y_true_pos, y_true_neg], dim=0).long()
-    return y_true_tot, y_pred_tot, anchor_tot
+    y_true_tot = torch.squeeze(torch.cat([y_true_pos, y_true_neg], dim=0))
+    return y_pred_tot, y_true_tot, anchor_tot
 
 
 @dataclass
@@ -38,13 +41,17 @@ class WeightedLoss:
     def __call__(self, y_pred, y_true, anchors):
         y_pred_encoded = self.enc_pred(y_pred, anchors)
         y_true_encoded = self.enc_true(y_true, anchors)
+        import ipdb; ipdb.set_trace(); import IPython; IPython.embed()  # noqa
         return self.weight * self.loss(y_pred_encoded, y_true_encoded)
 
 
 def default_losses():
     losses = {
         "boxes": WeightedLoss(torch.nn.MSELoss()),
-        "classes": WeightedLoss(torch.nn.CrossEntropyLoss()),
+        "classes": WeightedLoss(
+            torch.nn.CrossEntropyLoss(),
+            enc_true=lambda y, _: y.reshape(-1)
+        ),
     }
     return losses
 
@@ -70,9 +77,10 @@ class DetectionLoss(torch.nn.Module):
             # )
             # ~> y_pred_[n_samples, dim1]
             # ~> y_true_[n_samples, dim2]
-            # ~> anchor_[n_samples, dim2]
+            # ~> anchor_[n_samples, 4]
 
             y_pred_, y_true_, anchor_ = fselect(preds[name], y[name], anchors)
 
-            losses.append(subloss(y_pred_, y_true_, anchor_))
+            losses.append(subloss(y_pred_, y_true_.long(), anchor_))
+
         return torch.stack(losses).sum()
