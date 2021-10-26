5a14400795966c3d2d88bd1d52f4af8a6b2ad30f
kqf <q.dev.null@gmail.com>
2021-10-26 10:08:00 +0200

    Add encodings (#42)
    * Add encodings
    
    * Move to cchw to encodings module

---
+++ detectors/encode.py
@@ -0,0 +1,42 @@
+import torch
+
+
+def to_cchw(x):
+    cchw = x.clone().detach()
+    cchw[..., 0] = x[..., 0] - x[..., 2] / 2.
+    cchw[..., 1] = x[..., 1] - x[..., 3] / 2.
+    cchw[..., 2] = x[..., 0] + x[..., 2] / 2.
+    cchw[..., 3] = x[..., 1] + x[..., 3] / 2.
+    return cchw
+
+
+def encode(boxes, anchors):
+    x0y0 = boxes[..., :2]
+    x1y1 = boxes[..., 2:]
+
+    # Convert to cchw
+    wh = x1y1 - x0y0
+    cc = x0y0 + wh / 2.
+
+    # Apply nonlinearity
+    encoded_cc = (cc - anchors[:, 0:2]) / anchors[:, 2:4]
+    encoded_wh = torch.log(wh / anchors[:, 2:4])
+
+    encoded = torch.cat([encoded_cc, encoded_wh], dim=-1)
+    return encoded
+
+
+def decode(boxes, anchors):
+    encoded_cc = boxes[..., :2]
+    encoded_wh = boxes[..., 2:]
+
+    # Apply nonlinearity
+    cc = (encoded_cc * anchors[..., 2:]) + anchors[..., :2]
+    wh = torch.exp(encoded_wh) * anchors[..., 2:]
+
+    # Convert to 0011
+    x0y0 = cc - wh / 2.
+    x1y1 = x0y0 + wh
+
+    decoded = torch.cat([x0y0, x1y1], dim=-1)
+    return decoded
