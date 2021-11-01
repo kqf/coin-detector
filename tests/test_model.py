7eaf0d67cbb1a27913732b83bcd4e3c902c139a4
kqf <q.dev.null@gmail.com>
2021-11-01 06:18:54 +0100

    Parametrize number of epochs (#58)
    * Add the max epoch option to the file
    
    * Add max epochs to the output

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,11 +1,17 @@
+import pytest
 import pandas as pd
 from detectors.model import build_model
 from detectors.dataset import DetectionDataset
 
 
-def test_model(fake_dataset):
+@pytest.fixture
+def max_epochs(request):
+    return request.config.getoption("--max-epochs")
+
+
+def test_model(fake_dataset, max_epochs):
     df = pd.read_csv(fake_dataset / "train.csv")
     train = DetectionDataset(df)
 
-    model = build_model()
+    model = build_model(max_epochs=max_epochs)
     model.fit(train)
