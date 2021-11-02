1627ae8151f5e5a33fce60f826f09a044fca1e1f
Sasha <q.dev.null@gmail.com>
2021-11-02 13:14:38 +0100

    Fix the encoding -> for cchw

--- detectors/encode.py
+++ detectors/encode.py
@@ -35,8 +35,9 @@ def decode(boxes, anchors):
     wh = torch.exp(encoded_wh) * anchors[..., 2:]
 
     # Convert to 0011
-    x0y0 = cc - wh / 2.
-    x1y1 = x0y0 + wh
+    # x0y0 = cc - wh / 2.
+    # x1y1 = x0y0 + wh
+    # decoded = torch.cat([x0y0, x1y1], dim=-1)
 
-    decoded = torch.cat([x0y0, x1y1], dim=-1)
+    decoded = torch.cat([cc, wh], dim=-1)
     return decoded
