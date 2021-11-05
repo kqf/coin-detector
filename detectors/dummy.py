207646772d1c548cbe767f4f58c3c1606c78294f
kqf <q.dev.null@gmail.com>
2021-11-05 13:31:11 +0100

    Add the hidden layers (#67)
    * Try to fix the classification
    
    * Disable the regression loss
    
    * Add a single input layer
    
    * Increase the number of the hidden layers

--- detectors/dummy.py
+++ detectors/dummy.py
@@ -29,11 +29,15 @@ class DummyDetector(torch.nn.Module):
     def __init__(self, heads=None, n_classes=2, kernel_size=5):
         super().__init__()
         self.backbone = torch.nn.AdaptiveAvgPool2d(kernel_size)
+        self.hidden = torch.nn.Sequential(
+            torch.nn.Linear(kernel_size, 100),
+            torch.nn.Linear(100, kernel_size),
+        )
         self.heads = heads or default_heads(n_classes)
         self.anchors = AnchorBoxes()
 
     def forward(self, x):
-        latent = self.backbone(x)
+        latent = self.hidden(self.backbone(x))
         outputs = {n: h(latent) for n, h in self.heads.items()}
 
         _, _, *image_shape = x.shape
