60e6ae4fde6ab1ac223bf535bbe43ad48521a890
Sasha <q.dev.null@gmail.com>
2021-11-12 07:18:05 +0100

    Fix the coordinates

--- scripts/shapes.py
+++ scripts/shapes.py
@@ -2,13 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 
-from skimage.draw import disk
+from skimage.draw import disk, ellipse
 
 
 def box(cx, cy, w, h):
     ax = plt.gca()
     patch = patches.Rectangle(
-        (cx - w / 2, cy - h / 2), h, w,
+        (cx - w / 2, cy - h / 2), w, h,
         linewidth=1,
         edgecolor='r',
         facecolor='none'
@@ -16,8 +16,14 @@ def box(cx, cy, w, h):
     ax.add_patch(patch)
 
 
-def to_disc(img, cx, cy, h, w):
-    rr, cc = disk((cx, cy), h / 2., shape=img.shape)
+def to_disc(img, cx, cy, w, h):
+    rr, cc = disk((cy, cx), h / 2., shape=img.shape)
+    img[rr, cc] = 1
+    return img
+
+
+def to_ellipse(img, cx, cy, w, h):
+    rr, cc = ellipse(cy, cx, h / 2., w / 2., shape=img.shape)
     img[rr, cc] = 1
     return img
 
@@ -26,9 +32,8 @@ def main():
     shape = (400, 400)
     img = np.zeros(shape)
 
-    bbox = [200, 200, 50, 50]
-    img = to_disc(img, *bbox)
-    print(img)
+    bbox = [50, 50, 50, 80]
+    img = to_ellipse(img, *bbox)
     plt.imshow(img)
     box(*bbox)
     plt.show()
