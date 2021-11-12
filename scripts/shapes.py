38a8d4dfe3c0783e20dd16b782e223b933b813b4
Sasha <q.dev.null@gmail.com>
2021-11-12 08:58:40 +0100

    Check if works for rectangles

--- scripts/shapes.py
+++ scripts/shapes.py
@@ -2,14 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 
-from skimage.draw import disk, ellipse, rectangle
+from skimage.draw import disk, ellipse, rectangle, polygon
 
 
 def box(cx, cy, w, h):
     ax = plt.gca()
     patch = patches.Rectangle(
         (cx - w / 2, cy - h / 2), w, h,
-        linewidth=1,
+        linewidth=2,
         edgecolor='r',
         facecolor='none'
     )
@@ -37,6 +37,30 @@ def to_recatangle(img, cx, cy, w, h):
     return img
 
 
+def to_polygon(img, cx, cy, w, h):
+    # starts = (cy - h // 2, cx - w // 2)
+    # ends = (cy + h // 2, cx + w // 2)
+
+    starts = np.array([
+        cy - h // 2,
+        cy - h // 2,
+        cy + h // 2,
+        cy + h // 2,
+        # cy - h // 2,
+    ])
+    ends = np.array([
+        cx - w // 2,
+        cx + w // 2,
+        cx + w // 2,
+        cx - w // 2,
+        # cx - w // 2,
+    ])
+
+    rr, cc = polygon(starts, ends)
+    img[rr, cc] = 1
+    return img
+
+
 def main():
     shape = (400, 400)
     img = np.zeros(shape)
@@ -54,7 +78,7 @@ def main():
     box(*bbox)
 
     bbox = [350, 50, 50, 80]
-    img = to_ellipse(img, *bbox)
+    img = to_polygon(img, *bbox)
     box(*bbox)
 
     plt.imshow(img)
