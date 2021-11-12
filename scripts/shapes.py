30e2205232288258fc16a1895158b665d5acf551
Sasha <q.dev.null@gmail.com>
2021-11-12 07:37:31 +0100

    Add the rectangle

--- scripts/shapes.py
+++ scripts/shapes.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 
-from skimage.draw import disk, ellipse
+from skimage.draw import disk, ellipse, rectangle
 
 
 def box(cx, cy, w, h):
@@ -28,12 +28,25 @@ def to_ellipse(img, cx, cy, w, h):
     return img
 
 
+def to_recatangle(img, cx, cy, w, h):
+    start = (cy - h // 2, cx - w // 2)
+    end = (cy + h // 2, cx + w // 2)
+
+    rr, cc = rectangle(start, end, shape=img.shape)
+    img[rr, cc] = 1
+    return img
+
+
 def main():
     shape = (400, 400)
     img = np.zeros(shape)
 
     bbox = [50, 50, 50, 80]
     img = to_ellipse(img, *bbox)
+
+    bbox = [250, 250, 50, 80]
+    img = to_recatangle(img, *bbox)
+
     plt.imshow(img)
     box(*bbox)
     plt.show()
