8f8f717fdb5d92f19ddf30c9ede74dd28d731c1b
Sasha <q.dev.null@gmail.com>
2021-11-13 09:32:00 +0100

    Remove the nonexistent fimport

--- tests/test_mc.py
+++ tests/test_mc.py
@@ -1,11 +1,8 @@
 import matplotlib.pyplot as plt
-from detectors.mc import make_blob, blob2image
+from detectors.mc import make_blob
 
 
 def test_generates():
-    blob = make_blob()
+    blob = make_blob(class_id=1, h=400, w=400)
     plt.imshow(blob)
-
-    img = blob2image(blob)
-    plt.imshow(img)
     plt.show()
