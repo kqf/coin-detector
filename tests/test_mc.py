7203578f9a4b7644904ab4daaae519ab5deb4494
kqf <q.dev.null@gmail.com>
2021-11-13 09:37:05 +0100

    Fix broken generation (#87)
    * Simplify the dataset creation
    
    * Store two objects of different class
    
    * Remove the printouts
    
    * Remove the nonexistent fimport
    
    * Add the proper test

--- tests/test_mc.py
+++ tests/test_mc.py
@@ -1,11 +1,22 @@
+import pytest
 import matplotlib.pyplot as plt
-from detectors.mc import make_blob, blob2image
+from detectors.mc import make_image
 
 
-def test_generates():
-    blob = make_blob()
-    plt.imshow(blob)
+@pytest.fixture
+def blobs():
+    blobs = [
+        {
+            "x_center": 50,
+            "y_center": 50,
+            "width": 90,
+            "height": 90,
+        }
+    ]
+    return blobs
 
-    img = blob2image(blob)
-    plt.imshow(img)
+
+def test_generates(blobs):
+    image = make_image(shapes=blobs, image_shape=(400, 400))
+    plt.imshow(image)
     plt.show()
