0cfbe577f4d221d09314a15e52961e1b8cc3ecd7
kqf <q.dev.null@gmail.com>
2021-11-15 07:09:40 +0100

    Move primitives to a module (#88)
    * Add shapes module
    
    * Add missing dependency
    
    * Fix the shapes

--- scripts/shapes.py
+++ scripts/shapes.py
@@ -1,70 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.patches as patches
 
-from skimage.draw import disk, ellipse, rectangle, polygon
+from detectors.shapes import to_ellipse, to_recatangle, to_disc, to_polygon
 
-
-def box(cx, cy, w, h):
-    ax = plt.gca()
-    patch = patches.Rectangle(
-        (cx - w / 2, cy - h / 2), w, h,
-        linewidth=2,
-        edgecolor='r',
-        facecolor='none'
-    )
-    ax.add_patch(patch)
-
-
-def to_disc(img, cx, cy, w, h):
-    rr, cc = disk((cy, cx), h / 2., shape=img.shape)
-    img[rr, cc] = 1
-    return img
-
-
-def to_ellipse(img, cx, cy, w, h):
-    rr, cc = ellipse(cy, cx, h / 2., w / 2., shape=img.shape)
-    img[rr, cc] = 1
-    return img
-
-
-def to_recatangle(img, cx, cy, w, h):
-    start = (cy - h // 2, cx - w // 2)
-    end = (cy + h // 2, cx + w // 2)
-
-    rr, cc = rectangle(start, end, shape=img.shape)
-    img[rr, cc] = 1
-    return img
-
-
-def to_polygon(img, cx, cy, w, h, n=3, rot=0):
-    i = np.arange(n)
-
-    rows = cy + h / 2 * np.cos(2 * np.pi * i / n + rot)
-    cols = cx + w / 2 * np.sin(2 * np.pi * i / n + rot)
-
-    rr, cc = polygon(rows, cols, shape=img.shape)
-    img[rr, cc] = 1
-    return img
+from detectors.shapes import box
 
 
 def main():
     shape = (400, 400)
     img = np.zeros(shape)
 
-    bbox = [50, 50, 80, 50]
+    bbox = [350, 50, 80, 50]
     img = to_ellipse(img, *bbox)
     box(*bbox)
 
-    bbox = [350, 350, 50, 80]
+    bbox = [350, 150, 50, 80]
     img = to_recatangle(img, *bbox)
     box(*bbox)
 
-    bbox = [50, 350, 50, 80]
+    bbox = [350, 250, 50, 80]
     img = to_disc(img, *bbox)
     box(*bbox)
 
-    bbox = [350, 200, 50, 100]
+    bbox = [350, 350, 50, 100]
     img = to_polygon(img, *bbox)
     box(*bbox)
 
