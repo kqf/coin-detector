84e89a098b56e9e48caeb4e10e3b17dd23187533
Sasha <q.dev.null@gmail.com>
2021-11-22 06:32:19 +0100

    Fix the dataset schema

--- tests/test_mc.py
+++ tests/test_mc.py
@@ -7,18 +7,12 @@ from detectors.mc import make_image
 def blobs():
     blobs = [
         {
-            "x_center": 50,
-            "y_center": 50,
-            "width": 90,
-            "height": 90,
+            "bbox": [50, 50, 90, 90],
             "class_id": 0,
             "colors": [13, 46, 96],
         },
         {
-            "x_center": 150,
-            "y_center": 250,
-            "width": 90,
-            "height": 90,
+            "bbox": [150, 250, 90, 90],
             "class_id": 1,
             "colors": [94, 103, 36],
         },
