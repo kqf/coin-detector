879930d3f597129f926f4d67b80572a8a9fbe94e
Sasha <q.dev.null@gmail.com>
2021-11-23 06:20:42 +0100

    Remove make blob function

--- detectors/mc.py
+++ detectors/mc.py
@@ -1,5 +1,4 @@
 import numpy as np
-import pandas as pd
 
 
 def make_shape(
@@ -24,30 +23,6 @@ def make_colors(num_colors, channels=3, intensity_range=(0, 255)):
     colors = [np.random.randint(_min, _max, size=num_colors)
               for _min, _max in intensity_range]
     return np.transpose(colors)
-
-
-def make_blob(
-    x_center=50,
-    y_center=50,
-    width=90,
-    height=90,
-    channels=3,
-    epsilon=0.1,
-    class_id=0,
-    shape=(2000, 2000),
-    **kwargs
-):
-    blob = make_shape(x_center, y_center, width, height, shape)
-    h, w = blob.shape
-
-    extended = blob[..., None]
-    # return extended + 255
-
-    # Add a small term to add noise to the empty regions
-    noise = np.random.poisson(extended * epsilon, size=(h, w, channels))
-
-    # Convet to image scale
-    return (extended + class_id * noise * 255.).astype(np.uint8)
 
 
 def make_image(shapes, image_shape, channels=3, fmt="coco"):
