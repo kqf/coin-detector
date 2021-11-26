80715a64f05e585f56fd94a154c4d9f7d0233479
kqf <q.dev.null@gmail.com>
2021-11-26 18:08:46 +0100

    Improve shape coverage (#114)
    * Add shapes test'
    
    * Remove the scripts folder

--- tests/test_shapes.py
+++ tests/test_shapes.py
@@ -1,3 +1,4 @@
+import pytest
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -6,30 +7,30 @@ from detectors.shapes import to_ellipse, to_recatangle, to_disc, to_polygon
 from detectors.shapes import box
 
 
-def main():
+@pytest.fixture
+def image():
     shape = (400, 400)
     img = np.zeros(shape)
+    return img
 
+
+def test_shapes(image):
     bbox = [350, 50, 80, 50]
-    img = to_ellipse(img, *bbox)
+    image = to_ellipse(image, *bbox)
     box(*bbox)
 
     bbox = [350, 150, 50, 80]
-    img = to_recatangle(img, *bbox)
+    image = to_recatangle(image, *bbox)
     box(*bbox)
 
     bbox = [350, 250, 50, 80]
-    img = to_disc(img, *bbox)
+    image = to_disc(image, *bbox)
     box(*bbox)
 
     bbox = [350, 350, 50, 100]
-    img = to_polygon(img, *bbox)
+    image = to_polygon(image, *bbox)
     box(*bbox)
 
-    plt.imshow(img)
+    plt.imshow(image)
     box(*bbox)
     plt.show()
-
-
-if __name__ == '__main__':
-    main()
