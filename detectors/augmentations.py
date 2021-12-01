31225e5cc15efd55b0c5e408b0eb8a1ad53bd274
Sasha <q.dev.null@gmail.com>
2021-12-01 18:35:20 +0100

    Add the trainig no augmentations

--- detectors/augmentations.py
+++ detectors/augmentations.py
@@ -49,7 +49,10 @@ def pipeline(train=True, mean=None, std=None, size=32 * 13):
     train_transforms = []
     if train:
         train_transforms = [
-            alb.Flip(1)
+            # bad
+            # alb.Flip(1),
+            # bad
+            # alb.RandomRotate90(),
         ]
 
     return alb.Compose(
