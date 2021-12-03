3f3ecb77434256a42e81f002acc7c8000591c2d6
Sasha <q.dev.null@gmail.com>
2021-12-03 17:30:30 +0100

    Fix the brightness contrast

--- detectors/augmentations.py
+++ detectors/augmentations.py
@@ -68,8 +68,8 @@ def pipeline(train=True, mean=None, std=None, size=32 * 13):
             #     alb.IAAPiecewiseAffine(p=0.3),
             # ], p=0.3),
             # alb.HueSaturationValue(10, 15, 10),
-            alb.CLAHE(clip_limit=2),
-
+            # alb.CLAHE(clip_limit=2),
+            alb.RandomBrightnessContrast(),
         ]
 
     return alb.Compose(
