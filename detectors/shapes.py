51a3bffb6851d2cbeac7018d945312a4ba7962af
kqf <q.dev.null@gmail.com>
2021-12-09 07:02:14 +0100

    Check horizontal augmentations (#144)
    * Test the shaped augmentations
    
    * Switch to horizontal flip

--- detectors/shapes.py
+++ detectors/shapes.py
@@ -8,7 +8,7 @@ from functools import partial
 def box(img, cx, cy, w, h):
     imw, imh, *_ = img.shape
 
-    print("Box ", cx, cy, w, h)
+    print("Box >", cx, cy, w, h)
     ax = plt.gca()
     patch = patches.Rectangle(
         (cx + w / 2, cy - h * 1.5), w, h,
@@ -36,7 +36,7 @@ def box(img, cx, cy, w, h):
 def to_circle(img, cx, cy, w, h):
     shape = img.shape
     Y, X = np.ogrid[:shape[0], :shape[1]]
-    print("Circle ", cx, cy, w, h)
+    print("Circle ~>", cx, cy, w, h)
 
     xx = (X[..., None] - cx)
     yy = (Y[..., None] - cy)
