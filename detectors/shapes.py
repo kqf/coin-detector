bb6b1b5e2a2c546ce50dee1c2fe3d17e8c1341a3
Sasha <q.dev.null@gmail.com>
2021-12-12 17:22:59 +0100

    Remove the multiple locator

--- detectors/shapes.py
+++ detectors/shapes.py
@@ -2,7 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 
-from matplotlib.ticker import MultipleLocator
 from skimage.draw import disk, ellipse, rectangle, polygon
 from functools import partial
 
