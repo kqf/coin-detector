39243caaf2a0469ea8c5259146c2a11955893f3c
Sasha <q.dev.null@gmail.com>
2021-12-14 07:53:49 +0100

    Fix the comment on image

--- detectors/augmentations.py
+++ detectors/augmentations.py
@@ -50,8 +50,6 @@ def pipeline(train=True, mean=None, std=None, size=32 * 13):
     train_transforms = []
     if train:
         train_transforms = [
-            # It looks like horizaonatal flip doesn't alter the image
-            # But it changes the box annotations
             alb.HorizontalFlip(),
             # Vertical flips don't change the position of an image
             # they seem to change only annotations
