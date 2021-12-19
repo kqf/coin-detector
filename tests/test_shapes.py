ae5fd21f88d4861899862d64215df1f17e96f574
Sasha <q.dev.null@gmail.com>
2021-12-19 12:48:41 +0200

    Add the on-mask shapes

--- tests/test_shapes.py
+++ tests/test_shapes.py
@@ -13,11 +13,23 @@ def image():
     return img
 
 
+def on_box(image, cx, cy, w, h):
+    x0, x1 = cx - w / 2, cx + w / 2
+    y0, y1 = cy - h / 2, cy + h / 2
+
+    xx, yy = np.meshgrid(
+        np.arange(image.shape[0]),
+        np.arange(image.shape[1]),
+    )
+    mask = (x0 < xx) & (xx < x1) & (y0 < yy) & (yy < y1)
+    return mask
+
+
 @pytest.mark.parametrize("method", [
     to_ellipse,
-    to_recatangle,
-    to_disc,
-    to_polygon,
+    # to_recatangle,
+    # to_disc,
+    # to_polygon,
 ])
 def test_shapes(method, image):
     bbox = [150, 50, 80, 50]
@@ -25,3 +37,6 @@ def test_shapes(method, image):
     box(image, *bbox)
     plt.imshow(image)
     plt.show()
+    mask = on_box(image, *bbox)
+    assert image[mask].any()
+    assert not image[~mask].any()
