31a7c87d8fabaa1e8a252204e53df5b29eea43a9
kqf <q.dev.null@gmail.com>
2021-12-19 12:52:38 +0200

    Add automated shape checks (#168)
    * Add the on-mask shapes
    
    * Fix naming
    
    * Fix the broken condition

--- tests/test_shapes.py
+++ tests/test_shapes.py
@@ -13,11 +13,23 @@ def image():
     return img
 
 
+def box_mask(image, cx, cy, w, h):
+    x0, x1 = cx - w / 2, cx + w / 2
+    y0, y1 = cy - h / 2, cy + h / 2
+
+    xx, yy = np.meshgrid(
+        np.arange(image.shape[0]),
+        np.arange(image.shape[1]),
+    )
+    mask = (x0 <= xx) & (xx <= x1) & (y0 <= yy) & (yy <= y1)
+    return mask
+
+
 @pytest.mark.parametrize("method", [
     to_ellipse,
     to_recatangle,
-    to_disc,
-    to_polygon,
+    # to_polygon,
+    # to_disc,
 ])
 def test_shapes(method, image):
     bbox = [150, 50, 80, 50]
@@ -25,3 +37,6 @@ def test_shapes(method, image):
     box(image, *bbox)
     plt.imshow(image)
     plt.show()
+    within_box = box_mask(image, *bbox)
+    assert image[within_box].any(), "There is something within the box"
+    assert not image[~within_box].any(), "There is nothing outside the box"
