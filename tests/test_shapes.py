eed1cbe9d373837a20c937194624e3533b930ed2
Sasha <q.dev.null@gmail.com>
2021-12-23 07:57:24 +0200

    Release the tests

--- tests/test_shapes.py
+++ tests/test_shapes.py
@@ -39,9 +39,9 @@ def bbox(cx, cy, w, h):
 @pytest.mark.parametrize("cx", np.arange(0, 400, step=40))
 @pytest.mark.parametrize("cy", np.arange(0, 400, step=40))
 @pytest.mark.parametrize("w, h", [
-    # (80, 50),
+    (80, 50),
     (50, 80),
-    # (50, 50),
+    (50, 50),
 ])
 def test_shapes(method, image, bbox):
     image = method(image, *bbox)
