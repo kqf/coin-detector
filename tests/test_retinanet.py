1ee57cb2965b7b45d523bddbd3595f037e2a323f
Sasha <q.dev.null@gmail.com>
2021-12-30 07:56:23 +0200

    Fix the input parametrization

--- tests/test_retinanet.py
+++ tests/test_retinanet.py
@@ -4,10 +4,10 @@ from detectors.retinanet import FPN
 
 
 @pytest.fixture
-def layer_outputs():
-    x1 = torch.rand(1, 16, 64, 64)
-    x2 = torch.rand(1, 32, 32, 32)
-    x3 = torch.rand(1, 64, 16, 16)
+def layer_outputs(batch_size=4):
+    x1 = torch.ones(batch_size, 16, 64, 64)
+    x2 = torch.ones(batch_size, 32, 32, 32)
+    x3 = torch.ones(batch_size, 64, 16, 16)
     return x1, x2, x3
 
 
