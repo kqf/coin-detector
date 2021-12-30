2a736987a3ef347cf47b29d8d2523db2a40f08a1
Sasha <q.dev.null@gmail.com>
2021-12-30 08:07:14 +0200

    Improve the tests

--- tests/test_retinanet.py
+++ tests/test_retinanet.py
@@ -1,5 +1,6 @@
 import pytest
 import torch
+
 from detectors.retinanet import FPN
 
 
@@ -11,6 +12,17 @@ def layer_outputs(batch_size=4):
     return x1, x2, x3
 
 
-def test_fpn(layer_outputs):
-    model = FPN(16, 32, 64)
-    model(layer_outputs)
+def test_fpn(layer_outputs, feature_size=256):
+    model = FPN(16, 32, 64, feature_size=256)
+    o3, o4, o5 = layer_outputs
+    x3, x4, x5, x6, x7 = model(layer_outputs)
+
+    assert x3.shape[1] == 256
+    assert x4.shape[1] == 256
+    assert x5.shape[1] == 256
+    assert x6.shape[1] == 256
+    assert x7.shape[1] == 256
+
+    # assert x3.shape == o3.shape
+    # assert x4.shape == o4.shape
+    # assert x5.shape == o5.shape
