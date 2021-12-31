a41449e69acaa8651826895322cfd436c2a517d1
kqf <q.dev.null@gmail.com>
2021-12-31 10:31:04 +0200

    Check the numeric results (#198)
    * Improve the tests
    
    * Test shapes
    
    * Check the numerics
    
    * Add the retinanet comparison

--- tests/test_retinanet.py
+++ tests/test_retinanet.py
@@ -12,8 +12,24 @@ def layer_outputs(batch_size=4):
     return x1, x2, x3
 
 
+def initialize(model):
+    for p in model.parameters():
+        torch.nn.init.constant_(p, 1.)
+
+
+def expected(shape, fill, edge, corner):
+    x = torch.full(shape, fill)
+    x[:, :, :, [0, -1]] = x[:, :, [0, -1]] = edge
+    x[:, :, 0, 0] = edge
+    x[:, :, 0, -1] = edge
+    x[:, :, -1, 0] = edge
+    x[:, :, -1, -1] = edge
+    return x
+
+
 def test_fpn(layer_outputs, feature_size=256):
     model = FPN(16, 32, 64, feature_size=256)
+    initialize(model)
     o3, o4, o5 = layer_outputs
     x3, x4, x5, x6, x7 = model(layer_outputs)
 
@@ -27,3 +43,6 @@ def test_fpn(layer_outputs, feature_size=256):
 
     assert x6.shape == (4, feature_size, 8, 8)
     assert x7.shape == (4, feature_size, 4, 4)
+
+    assert torch.testing.assert_allclose(
+        x3, expected((4, 256, 64, 64), 264961., 117761, 176641))
