Fix the expected tensor calculation

tests/test_retinanet.py
@@ -20,10 +20,10 @@ def initialize(model):
 def expected(shape, fill, edge, corner):
     x = torch.full(shape, fill)
     x[:, :, :, [0, -1]] = x[:, :, [0, -1]] = edge
-    x[:, :, 0, 0] = edge
-    x[:, :, 0, -1] = edge
-    x[:, :, -1, 0] = edge
-    x[:, :, -1, -1] = edge
+    x[:, :, 0, 0] = corner
+    x[:, :, 0, -1] = corner
+    x[:, :, -1, 0] = corner
+    x[:, :, -1, -1] = corner
     return x
 
 
@@ -44,5 +44,6 @@ def test_fpn(layer_outputs, feature_size=256):
     assert x6.shape == (4, feature_size, 8, 8)
     assert x7.shape == (4, feature_size, 4, 4)
 
-    # assert torch.testing.assert_allclose(
-    #     x3, expected((4, 256, 64, 64), 264961., 117761, 176641))
+    x3_exp = expected((4, 256, 64, 64), 264961., 176641, 117761)
+    import ipdb; ipdb.set_trace(); import IPython; IPython.embed() # noqa
+    torch.testing.assert_allclose(x3, x3_exp)