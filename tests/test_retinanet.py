2039fc1c1f83f06bec37d97989daa3c5029e5fc8
Sasha <q.dev.null@gmail.com>
2022-01-07 06:27:40 +0200

    Convert to floats

--- tests/test_retinanet.py
+++ tests/test_retinanet.py
@@ -98,7 +98,7 @@ def test_retinanet(batch, output_features=256):
     initialize(model)
     x3, x4, x5, x6, x7 = model(batch)
 
-    assert len(torch.unique(x3)) == 1141
+    assert len(torch.unique(x3.long())) == 1122
     assert x3.shape == (4, output_features, 60, 60)
     assert x4.shape == (4, output_features, 30, 30)
     assert x5.shape == (4, output_features, 15, 15)
