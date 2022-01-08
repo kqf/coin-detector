9bacb650ee2c5e48cd6637d181cd07643ff6a10d
kqf <q.dev.null@gmail.com>
2022-01-08 18:51:53 +0200

    Speedup the tests (#218)
    * Remove the parametrization
    
    * Fix the broken tests

--- tests/test_retinanet.py
+++ tests/test_retinanet.py
@@ -90,11 +90,10 @@ def test_default_fpn(layer_outputs, feature_size=256):
 
 
 @pytest.fixture
-def batch(image_size, batch_size=4):
+def batch(image_size=480, batch_size=4):
     return torch.ones(batch_size, 3, image_size, image_size)
 
 
-@pytest.mark.parametrize("image_size", [480, 2 * 480, 560])
 def test_retinanet(batch, output_features=256):
     model = RetinaNet(out_channels=output_features)
     initialize(model)
