3bdf4118b11b5f4186eb1d55f112e7f71945cb39
kqf <q.dev.null@gmail.com>
2022-01-15 07:36:10 +0100

    Add mobilenet version of retinanet (#234)
    * Add the mobilenet version
    
    * Add the tests

--- detectors/retinanet.py
+++ detectors/retinanet.py
@@ -1,9 +1,10 @@
 import torch
 from torchvision.models import resnet50
 from torchvision.models._utils import IntermediateLayerGetter
+from torchvision.models.detection.backbone_utils import mobilenet_backbone
 
+from detectors.anchors import DEFAULT_ANCHORS, AnchorBoxes
 from detectors.dummy import default_heads
-from detectors.anchors import AnchorBoxes, DEFAULT_ANCHORS
 
 
 class PyramidBlock(torch.nn.Module):
@@ -127,4 +128,41 @@ class RetinaNet(torch.nn.Module):
 
         _, _, *image_shape = x.shape
         acnhors, _ = self.anchors(image_shape, pyramids)
+        return outputs, acnhors
+
+
+class MobileRetinaNet(torch.nn.Module):
+    def __init__(
+            self,
+            layer_idx=None,
+            out_channels=256,
+            n_classes=2,
+            kernel_size=1,
+            pretrained=True,
+            anchors=None,
+    ):
+        super().__init__()
+        self.fpn = mobilenet_backbone(
+            "mobilenet_v2",
+            pretrained=pretrained,
+            fpn=True
+        )
+        self.heads = default_heads(
+            n_classes=n_classes,
+            channels=out_channels,
+            kernel_size=kernel_size,
+        )
+        self.anchors = AnchorBoxes(anchors or DEFAULT_ANCHORS * 5)
+
+    def forward(self, x):
+        pyramids = self.fpn(x)
+        import ipdb; ipdb.set_trace(); import IPython; IPython.embed() # noqa
+
+        outputs = {
+            name: torch.cat([h(x) for x in pyramids.values()], dim=1)
+            for name, h in self.heads.items()
+        }
+
+        _, _, *image_shape = x.shape
+        acnhors, _ = self.anchors(image_shape, pyramids.values())
         return outputs, acnhors
