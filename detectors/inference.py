5edb38fd6f319e5bd3ae4e39bb638cd91a5868a1
Sasha <q.dev.null@gmail.com>
2022-01-26 19:15:34 +0100

    Add the inference modules

---
+++ detectors/inference.py
@@ -0,0 +1,22 @@
+from detectors.iou import iou
+
+
+def nms(bboxes, anchors, threshold=0.5, min_iou=0.5, top_n=None):
+    # Filter out the boxes with low objectness score
+    x = bboxes[bboxes[:, 0] > threshold]
+
+    # Ensure everything is calculated per class
+    same_object = x[:, None, -1] == x[None, :, -1]
+
+    # Find non-maximum elements
+    objectness_per_class = same_object * x[None, :, 0]
+    maximum = objectness_per_class.max(-1, keepdim=True).values
+    not_maximum = objectness_per_class < maximum
+
+    # IoUs
+    ious = iou(boxes, anchors)
+
+    # Putting it all together
+    noise = same_object * not_maximum * (ious > min_iou).squeeze(-1)
+    suppressed = (~noise).all(0)
+    return x[suppressed, 1:]
