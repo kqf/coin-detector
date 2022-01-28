05931252ed4158574ff854fe4c9b8e71328aa724
Sasha <q.dev.null@gmail.com>
2022-01-28 07:19:27 +0100

    Fix the number of candidates

--- tests/test_nms.py
+++ tests/test_nms.py
@@ -6,19 +6,19 @@ from detection.inference import nms
 
 
 @pytest.fixture
-def candidates(n_candidates=13 * 3 + 26 * 3 + 52 * 3):
-    x = np.zeros((n_candidates, 4))
-    x[:, 0] = np.linspace(0.4, 0.6, n_candidates)
-    x[n_candidates // 2, 0] = 1
+def candidates(n_anchors=400):
+    x = np.zeros((n_anchors, 4))
+    x[:, 0] = np.linspace(0.4, 0.6, n_anchors)
+    x[n_anchors // 2, 0] = 1
 
-    x[:, 1] = np.linspace(0.4, 0.5, n_candidates)
-    x[:, 2] = np.linspace(0.4, 0.5, n_candidates)
+    x[:, 1] = np.linspace(0.4, 0.5, n_anchors)
+    x[:, 2] = np.linspace(0.4, 0.5, n_anchors)
     x[:, 3] = 0.2
 
     predictions = {}
     predictions["boxes"] = torch.tensor(x)
 
-    classes = np.zeros((n_candidates, 4))
+    classes = np.zeros((n_anchors, 4))
     predictions["classes"] = torch.tensor(classes)
     return predictions
 
