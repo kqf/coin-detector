bc6daa2546c6d86f9266aa4dff7d4a64ed99b8ee
kqf <q.dev.null@gmail.com>
2022-01-31 07:03:23 +0100

    Fix the infer fixture (#274)
    * Fix the tests
    
    * Fix the infer function

--- tests/test_inference.py
+++ tests/test_inference.py
@@ -8,10 +8,10 @@ from detectors.inference import infer
 @pytest.fixture
 def candidates(batch_size=4, n_anchors=400, n_classes=4):
     x = np.zeros((batch_size, n_anchors, 4))
-    x[:, 0] = np.linspace(0.4, 0.6, n_anchors)
-    x[:, 1] = np.linspace(0.4, 0.5, n_anchors)
-    x[:, 2] = np.linspace(0.4, 0.5, n_anchors)
-    x[:, 3] = 0.2
+    x[..., 0] = np.linspace(0.4, 0.6, n_anchors)
+    x[..., 1] = np.linspace(0.4, 0.5, n_anchors)
+    x[..., 2] = np.linspace(0.4, 0.5, n_anchors)
+    x[..., 3] = 0.2
 
     predictions = {}
     predictions["boxes"] = torch.tensor(x)
@@ -21,7 +21,7 @@ def candidates(batch_size=4, n_anchors=400, n_classes=4):
     return predictions
 
 
-@pytest.mark.skip
+# @pytest.mark.skip
 def test_inference(candidates):
     sup = infer(candidates)
     print(len(sup))
