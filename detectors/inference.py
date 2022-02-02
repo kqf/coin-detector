cf1e0eaefc4d3bf0498fb5d4250c994ce748c73b
Sasha <q.dev.null@gmail.com>
2022-02-02 19:14:57 +0100

    Remove the unused code

--- detectors/inference.py
+++ detectors/inference.py
@@ -1,4 +1,3 @@
-import numpy as np
 from torchvision.ops import batched_nms
 
 
@@ -10,9 +9,3 @@ def infer(batch, thr=0.5, **kwargs):
         scores_, class_ids_ = classes_.max(dim=-1)
         predictions.append(batched_nms(boxes_, scores_, class_ids_, thr))
     return predictions
-
-
-def one_hot(data, n_classes):
-    encoded = np.zeros((data.shape[0], n_classes))
-    encoded[np.arange(data.shape[0]), data] = 1
-    return encoded
