c1cd9e6100b340e4fdc07ed92393aaee0ba7118e
Sasha <q.dev.null@gmail.com>
2022-02-03 19:08:16 +0100

    Fix the anchors

--- detectors/inference.py
+++ detectors/inference.py
@@ -6,8 +6,8 @@ def infer(batch, thr=0.5, **kwargs):
     predictions = []
     preds, anchors = batch
     boxes, classes = preds["boxes"], preds["classes"]
-    for boxes_, classes_ in zip(boxes, classes):
+    for boxes_, classes_, anchors_ in zip(boxes, classes, anchors):
         scores_, class_ids_ = classes_.max(dim=-1)
-        decoded = decode(boxes_, anchors)
+        decoded = decode(boxes_, anchors_)
         predictions.append(batched_nms(decoded, scores_, class_ids_, thr))
     return predictions
