b00a41284b5cc6af708f598eb87798c111c722e6
kqf <q.dev.null@gmail.com>
2022-02-04 07:50:41 +0100

    Fix the output encoding (#282)
    * Decode the boxes
    
    * Fix the anchors

--- detectors/inference.py
+++ detectors/inference.py
@@ -1,11 +1,13 @@
 from torchvision.ops import batched_nms
+from detectors.encode import decode
 
 
 def infer(batch, thr=0.5, **kwargs):
     predictions = []
     preds, anchors = batch
     boxes, classes = preds["boxes"], preds["classes"]
-    for boxes_, classes_ in zip(boxes, classes):
+    for boxes_, classes_, anchors_ in zip(boxes, classes, anchors):
         scores_, class_ids_ = classes_.max(dim=-1)
-        predictions.append(batched_nms(boxes_, scores_, class_ids_, thr))
+        decoded = decode(boxes_, anchors_)
+        predictions.append(batched_nms(decoded, scores_, class_ids_, thr))
     return predictions
