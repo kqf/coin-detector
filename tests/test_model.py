d564ecf24900d5a38cb1aa4b1924b1dfd10a5e60
Sasha <q.dev.null@gmail.com>
2022-02-05 13:36:02 +0100

    Add the plotting functions'

--- tests/test_model.py
+++ tests/test_model.py
@@ -3,6 +3,9 @@ from detectors.model import build_model
 from detectors.dataset import DetectionDataset, read_dataset
 from detectors.augmentations import transform
 
+from matplotlib import pyplot as plt
+from detectors.shapes import box, arrows
+
 
 @pytest.fixture
 def max_epochs(request):
@@ -15,4 +18,13 @@ def test_model(fake_dataset, max_epochs):
 
     model = build_model(max_epochs=max_epochs)
     model.fit(train)
-    model.predict_proba(train)
+    predictions = model.predict_proba(train)
+
+    # Now visually check the results
+    for image, preds in zip(train, predictions):
+        channels_last = image.cpu().numpy().transpose(1, 2, 0)
+        for coords in preds:
+            box(channels_last, *coords)
+        plt.imshow(channels_last)
+        arrows()
+        plt.show()
