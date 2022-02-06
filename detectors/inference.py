3b68d4736eada450680fbae41b118260a640d9e4
kqf <q.dev.null@gmail.com>
2022-02-06 19:00:58 +0100

    Reformat outputs (#288)
    * Add cchw conversion
    
    * Add the missing inference script

--- detectors/inference.py
+++ detectors/inference.py
@@ -1,4 +1,5 @@
 from torchvision.ops import batched_nms
+from detectors.encode import to_cchw
 
 
 def infer(batch, decode, threshold=0.5, **kwargs):
@@ -9,5 +10,5 @@ def infer(batch, decode, threshold=0.5, **kwargs):
         scores_, class_ids_ = classes_.max(dim=-1)
         decoded = decode(boxes_, anchors_)
         selected = batched_nms(decoded, scores_, class_ids_, threshold)
-        predictions.append((decoded[selected], class_ids_[selected]))
+        predictions.append((to_cchw(decoded[selected]), class_ids_[selected]))
     return predictions
