1b5935a3c6863dbdeba163dbae0ba49607fb6e5f
Sasha <q.dev.null@gmail.com>
2022-02-07 19:44:30 +0100

    Remove optimizer

--- detectors/model.py
+++ detectors/model.py
@@ -42,7 +42,7 @@ def build_model(max_epochs=2, logdir=".tmp/", train_split=None):
         lr=base_lr,
         criterion=DetectionLoss,
         criterion__sublosses=sublosses,
-        optimizer=torch.optim.Adam,
+        # optimizer=torch.optim.Adam,
         # optimizer__momentum=0.9,
         iterator_train__shuffle=True,
         iterator_train__num_workers=6,
