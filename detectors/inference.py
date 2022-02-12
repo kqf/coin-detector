643b21cdf805d9d127b2dfd7c9da9ce6de51cb06
Sasha <q.dev.null@gmail.com>
2022-02-12 11:19:30 +0100

    Change the threshold

--- detectors/inference.py
+++ detectors/inference.py
@@ -1,9 +1,8 @@
 from torchvision.ops import batched_nms
+from detectors.encode import to_cchw
 
-from detectors.encode import to_coords
 
-
-def infer(batch, decode, threshold=0.5, **kwargs):
+def infer(batch, decode, threshold=0.9, **kwargs):
     predictions = []
     preds, anchors = batch
     boxes, classes = preds["boxes"], preds["classes"]
@@ -11,7 +10,5 @@ def infer(batch, decode, threshold=0.5, **kwargs):
         scores_, class_ids_ = classes_.max(dim=-1)
         decoded = decode(boxes_, anchors_)
         selected = batched_nms(decoded, scores_, class_ids_, threshold)
-        output_boxes = to_coords(decoded[selected])
-        predictions.append((output_boxes, class_ids_[selected]))
-
+        predictions.append((to_cchw(decoded[selected]), class_ids_[selected]))
     return predictions
